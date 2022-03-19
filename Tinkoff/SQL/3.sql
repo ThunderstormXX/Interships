f4aa2e810ce43551f715258219c1150611fbc5d6
Ignashin Igor <87493200+ThunderstormXX@users.noreply.github.com>
2022-03-19 21:15:08 +0300

    Update 3.sql

--- Tinkoff/SQL/3.sql
+++ Tinkoff/SQL/3.sql
@@ -1,9 +1,9 @@
-select top(10) name 
+select top(10) Пилоты.name 
 from Пилоты 
     left join Рейсы 
         on Пилоты.pilot_id = Рейсы.first_pilot_id 
     left join Самолёты 
         on Рейсы.plane_id = Самолёты.plane_id 
 where  Самолёты.cargo_flg = 1
-group by name 
+group by Пилоты.name 
 order by count(Самолеты.capacity) desc
